159fba5b6b5bf8e88cbaab1fbc5f41cfb51d57e8
Mirza Abdul Awal <84652300+mirzadev@users.noreply.github.com>
2023-07-02 11:38:56 -0400

    Gallery page created

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import karigorLogo from "./images/karigor-logo.PNG";
 import { capitalizeFirstLetter } from "../../utils/helper";
 
 function Nav() {
-  const categories = [
+  const [categories] = useState([
     {
       name: "Cultural",
       description:
@@ -14,22 +14,17 @@ function Nav() {
       description:
         "Today's childrens are our future, who will uphold the Bengali culture in future in USA",
     },
-    { name: "Education", description: "Delicious delicacies" },
     {
       name: "Social Activities",
       description:
         "Besides organizing annual cultural activities and events, Karigor is comitted to assist organizing social activities of other Bangladeshi organizarions in USA.",
     },
-  ];
-
-  const handleClick = () => {
-    console.log("click handled");
-  };
-
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
-        <a className="navbar-brand" id="brandName" href="/">
+        <a data-testid="link" href="/">
           <span>
             <img
               id="karigor-logo"
@@ -38,7 +33,7 @@ function Nav() {
               height="70"
               width="110"
             ></img>
-          </span>{" "}
+          </span>
         </a>
       </h2>
       <a className="title-header">
@@ -50,7 +45,7 @@ function Nav() {
             <a href="#home">Home</a>
           </li>
           <li className="mx-2">
-            <a href="#about">About</a>
+            <a href="#about">About Us</a>
           </li>
           <li className="mx-2">
             <a href="#contact">Contact Us</a>
@@ -58,11 +53,19 @@ function Nav() {
           <li className="mx-2">
             <a href="#events">Events</a>
           </li>
+          <li className="mx-2">
+            <a href="#gallery">Gallery</a>
+          </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
-                  handleClick();
+                  setCurrentCategory(category);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
@@ -74,74 +77,4 @@ function Nav() {
     </header>
   );
 }
-
-/*
-function Nav() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of Pohela Boishakh, cultural events, social activities",
-    },
-    {
-      name: "children",
-      description:
-        "Childrens of Karigor regularly gets training on Bangali culture and regularly takes part in social activities",
-    },
-    {
-      name: "religion",
-      description: "Karigor upholds the religious and ethnic values for all",
-    },
-    {
-      name: "mix-pictures",
-      description: "Pictures of various events",
-    },
-  ];
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
-  return (
-    <header>
-      <h2>
-        <a className="navbar-brand" id="brandName" href="/">
-          <span>
-            <img
-              id="karigor-logo"
-              src={karigorLogo}
-              alt=""
-              height="60"
-              width="100"
-            ></img>
-          </span>{" "}
-          KARIGOR
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#home">Home</a>
-          </li>
-          <li className="mx-2">
-            <a href="#about">About</a>
-          </li>
-          <li className="mx-2">
-            <a href="#contact">Contact Us</a>
-          </li>
-          <li className="mx-2">
-            <a href="#events">Events</a>
-          </li>
-          {categories.map((category) => {
-            <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
-              </span>
-            </li>;
-          })}
-        </ul>
-      </nav>
-    </header>
-  );
-}
-*/
-
 export default Nav;
